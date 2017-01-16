70360e0a45165415c0d29b5f40a54a5f76cb29de
doguz <selimdoguz@gmail.com>
2017-01-16 22:26:27 +0200

    c merhabadunya
    C dili için ekrana çıkış işlemi yaptıran kodu içerir.

---
+++ MerhabaDunya.c
@@ -0,0 +1,33 @@
+// bu satýrlar gibi satýr baþýna art arda iki adet "/" konulan satýrlar kod olarak çalýþtýrýlmaz.
+// "include" içermek, katmak, yer vermek manasýna gelir.
+// "#include" hazýr kod kütüphaneleri eklemeye yarar. 
+// "<stdio.h>" .h uzantýlý bir dosyadýr. bu tür dosyalar bilgisayar dilinde  
+// "stdio" standart(std) input(i) output(o) - standart giriþ çýkýþ kütüphanesi anlamýna gelir.
+#include <stdio.h>
+
+
+// "main()" bir fonksiyondur.
+// fonksiyonlarýn gövdesi süslü parantezler {} içinde yer alýr.
+// fonksiyonlarýn özellikleri isimlerin hemen yanýndaki parantez içine yazýlýr
+int main(){
+	
+	
+	// "printf" çýkýþ iþlemidir. ekrana metin ya da deðiþken deðeri basmaya yarar.
+	// týrnak içindekiler ekrana basýlýr.
+	printf("merhaba dunya");
+	
+	
+	return 0;
+}
+
+/*
+
+Ekrana Merhaba Dünya Yazdýran kodu yazýnýz.
+
+Added by BilgisayarKavramlari
+Örnek Ekran Çýktýsý 1
+merhaba dunya
+
+Added by BilgisayarKavramlari
+
+*/
